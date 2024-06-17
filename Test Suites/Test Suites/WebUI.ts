6d1c4c51a03c84e9b1f449132c0b0164783f0496
NhanNTM1 <mynhan1710@gmail.com>
2024-06-17 09:03:27 +0700

    add some test
    katalon test caset
    
    ver 2.0.0

---
+++ Test Suites/Test Suites/WebUI.ts
@@ -0,0 +1,21 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>WebUI</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>3dd2a954-7f08-4b49-b1c8-726b3695a526</testSuiteGuid>
+   <testCaseLink>
+      <guid>b98bdd7c-400a-40ab-bbd2-e31211e09977</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/TC01</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
